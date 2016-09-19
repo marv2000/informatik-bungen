c47d62e38573d7cd9386472cb8667c503adaca25
Grass.Marvin <Grass.Marvin@SK-448A5BCB8A89.2631.skool.net>
2016-09-19 15:11:15 +0200

    loop

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -3,7 +3,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Hello World");
+		int x =1;
+		while (true)
+		{
+			
+			System.out.print(x +" Hello World \n");
+			x=x+1;
+		}
+
 	}
 
 }
