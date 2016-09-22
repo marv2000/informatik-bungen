04b7a864dfaa2e16d98f059215afe51ca782bf79
MarV <MarV@PCMG001.fritz.box>
2016-09-22 15:58:42 +0200

    dawda

--- Hello World/src/zahlen.java
+++ Hello World/src/zahlen.java
@@ -4,6 +4,27 @@ static int x1=12;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		
+	int n = 5;
 
+	
+	
+	for (int i = 2; i <= n; i++) {
+		boolean isPrimzahl = true;
+			for (int j = 2; j < i && isPrimzahl; j++) {
+				if ((i % j) == 0) {
+					isPrimzahl = false;
+					}
+				}
+
+	
+			
+	if (isPrimzahl) {
+		System.out.println(i + " ist eine Primzahl!");
+				}
+
+		}
+	
+	}
+	
 }
