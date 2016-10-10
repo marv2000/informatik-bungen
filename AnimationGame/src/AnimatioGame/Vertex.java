caf3a6be426357f7f2a0e5b7e23f2e9457844550
Grass.Marvin <Grass.Marvin@SK-448A5BCB8A89.2631.skool.net>
2016-10-10 15:35:18 +0200

    dsad

--- AnimationGame/src/AnimatioGame/Vertex.java
+++ AnimationGame/src/AnimatioGame/Vertex.java
@@ -16,19 +16,25 @@ public class Vertex {
 //	Mainmethode
 public static void main(String[] args) {
 	
+	String s1="hallo";
+	String s2="HallO". toLowerCase();
+	
 	
 	Vertex v1 = new Vertex(15,76);
 	Vertex v2 = new Vertex(0,0);
 	Vertex v3 = new Vertex(5,6);
 	Vertex v4 =(v1.skalarMult(2));
 	Vertex v5=(v3.add(v1));
-
 	
-	System.out.println("v1.x= "+ v1.y);
+	v2.setXY(v1.x,v1.y);
+	
+	
+	
+	System.out.println("v1.x= "+ v1.x);
 	System.out.println("v1 "+ v1);
 	System.out.println(v1.length()+"\n");
 	
-	System.out.println("v2.x= "+ v2.y);
+	System.out.println("v2.x= "+ v2.x);
 	System.out.println("v2 "+ v2);
 	System.out.println(v2.length()+"\n");
 	
@@ -38,6 +44,27 @@ public static void main(String[] args) {
 
 	System.out.println("v4"+v4+"\n");
 	System.out.println("v5"+v5+"\n");
+
+	System.out.println(v1.x);
+	System.out.println(v1.y +"\n");
+
+	
+	System.out.println(s1);
+	System.out.println(s2);
+	System.out.println(s1==s2);
+	System.out.println(s1.equals(s2)+"\n");
+	
+	System.out.println(v1.equals(v2));
+	System.out.println();
+	System.out.println();
+	System.out.println();
+
+
+
+
+
+
+
 
 	}
 	
@@ -62,4 +89,37 @@ public static void main(String[] args) {
 		y=y+v2.y;
 	}
 	
+	public void setX(double x) {this.x = x;} //veränder der x/y parameter
+	
+	public void setY(double y) {this.y = y;}
+	
+	public void setXY(double x,double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {return x;}
+	
+	public double gety() {return y;}
+	
+	
+	public boolean equals(Object thatObject){
+		if (thatObject instanceof Vertex) {
+			Vertex that = (Vertex)thatObject;
+			return this.x==that.x && this.y == that.y;
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
 }
+
+
+
+
+
+
+
