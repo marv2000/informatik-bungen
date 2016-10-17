f0e3754ec6f9fc650cbbef6a6b79b96bfeff880d
Grass.Marvin <Grass.Marvin@SK-448A5BCB8A89.2631.skool.net>
2016-10-17 15:00:57 +0200

    fibonaccifolge

---
+++ AnimationGame/src/fibonacci.java
@@ -0,0 +1,26 @@
+
+public class fibonacci {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	double a=0;
+	double b=1;
+	double c=0;
+
+		while (c<1.3E308){
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+		}
+	
+
+
+	}
+	
+	
+	
+	
+	
+}
